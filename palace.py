0d195efd79618d5734fece7bd29ddf54ee1ab9c0
lchu7 <lchu7@u.rochesr.edu>
2021-06-05 14:29:59 -0400

    palace

---
+++ palace.py
@@ -0,0 +1,43 @@
+import json
+
+class Exit:  
+    def __init__(self, name, room_id):
+        self._name = name
+        self._room_id = room_id
+    
+    def get_name(self):
+        return self._name
+    
+    def get_room_id(self):
+        return self._room_id
+
+    def set_name(self, new_name):
+        self._name = new_name
+
+    def set_room_id(self, new_room_id):
+        self._room_id = new_room_id
+
+class Room:
+    def __init__(self, room_id, room_description, exits):
+        self._room_id = room_id
+        self._room_description = room_description
+        self._exits = exits
+        
+    def get_room_id(self):
+        return self._room_id
+
+    def get_room_description(self):
+        return self._room_description
+
+    def get_exits(self):
+        return self._exits
+
+    def set_room_id(self, new_room_id):
+        self._room_id = new_room_id
+
+    def set_room_description(self, new_room_description):
+        self._room_description = new_room_description
+
+    def set_exits(self, new_exits):
+        self._exits = new_exits
+        
