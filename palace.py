e90ae167168dba10326d873df868b27cea0b7cd0
Julianne <jms922@cornell.edu>
2021-06-05 20:51:43 -0400

    next_room function (copied OCaml code)

--- palace.py
+++ palace.py
@@ -41,3 +41,40 @@ class Room:
     def set_exits(self, new_exits):
         self._exits = new_exits
         
+class Adventure:
+    def __init__(self, rooms, starting_room):
+        self._rooms = rooms
+        self._starting_room = starting_room
+
+    def get_rooms(self):
+        return self._rooms
+    def get_starting_room(self):
+        return self._starting_room
+
+class UnknownRoom(Exception):
+    pass
+
+class UnknownExit(Exception):
+    pass
+
+def get_exits(room_list, room):
+    exits = []
+    for i in room_list:
+        if i.get_room_id() == room:
+            exits = i.get_exits()
+    if exits == []:
+        raise UnknownRoom
+    return exits
+def next_room_helper(exit_list, exit):
+    room = ""
+    for i in exit_list:
+        if i.get_name() == exit:
+            room = i.get_room_id()
+    if room == "":
+        raise UnknownExit
+    return room
+
+def next_room(adv, room, ex):
+    rooms = adv.get_rooms()
+    exit_list = get_exits(rooms, room)
+    return next_room_helper(exit_list, ex)
