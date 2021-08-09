012ffbedcc806175a8affcf6cc471fb9d3b98216
Julianne <jms922@cornell.edu>
2021-08-08 23:55:25 -0400

    Working maybe?

--- main.py
+++ main.py
@@ -5,30 +5,38 @@ from palace import *
 adventure = Adventure()
 state = State(rooms["start_room"], rooms["start_room"])
 
-#➥ Get Room
+# ➥ Get Room
+
+
 def get_room_object(room_id_input):
     room_object_list = [
         room_id for room_id in adventure.room_object_list if room_id.room_id == room_id_input]
     return room_object_list[0]
 ##
-#➥ Get Item
+# ➥ Get Item
+
+
 def get_item_object(item_name_input):
     item_object_list = [
         item for item in adventure.items if item.name == item_name_input]
     return item_object_list[0]
 ##
 
-#➥ Soup Dictionary
+
+# ➥ Soup Dictionary
 SoupDictionary = {
-    "carrot" : ", carrot",
-    "goldfish" : ", goldfish",
-    "pretzel" : ", pretzel"
+    "carrot": ", carrot",
+    "goldfish": ", goldfish",
+    "pretzel": ", pretzel"
 }
 ##
 # Puzzle(puzzle_type, room_object, goal_items)
-SoupPuzzle = Puzzle("soup", get_room_object("soup"), [get_item_object("carrot"), get_item_object("goldfish"), get_item_object("pretzel")], SoupDictionary)
+SoupPuzzle = Puzzle("soup", get_room_object("soup"), [get_item_object(
+    "carrot"), get_item_object("goldfish"), get_item_object("pretzel")], SoupDictionary)
 
-#➥ Functions for the puzzles of type 'soup'
+# ➥ Functions for the puzzles of type 'soup'
+
+
 def soup_puzzle_complete(puzzle):
     complete = True
     for i in puzzle.goal_items:
@@ -36,16 +44,21 @@ def soup_puzzle_complete(puzzle):
             complete = False
     return complete
 
+
 def update_descriptions(puzzle):
     for item in puzzle.goal_items:
         if item.room_id == puzzle.room_object.room_id:
             if not item.is_invisible:
-                puzzle.room_object.set_description(puzzle.room_object.description + puzzle.item_dictionary.get(item.name))
-                puzzle.room_object.set_look(puzzle.room_object.description + puzzle.item_dictionary.get(item.name))
+                puzzle.room_object.set_look(
+                    puzzle.room_object.description + puzzle.item_dictionary.get(item.name))
+                puzzle.room_object.set_description(
+                    puzzle.room_object.description + puzzle.item_dictionary.get(item.name))
             item.set_invisible(True)
 ##
 
-#➥ Parse Command
+# ➥ Parse Command
+
+
 def parse(inputCommand):
     # go north
     parseInput = inputCommand.lower().split(" ")
@@ -73,10 +86,10 @@ def parse(inputCommand):
                     state.current_room = current_room
                     new_room_object = room_object
                     print("This door is locked!")
-                    
+
                 if current_room != state.current_room:
                     print(new_room_object.description)
-                    
+
                 # Death Screen / Eject Room
                 if state.current_room == "ejectRoom":
                     return False
@@ -85,7 +98,7 @@ def parse(inputCommand):
             for item in adventure.items:
                 if state.current_room == item.room_id and item.name not in state.inventory and not item.is_invisible:
                     print("--" + item.description)
-                 
+
         # Room is not recognized
         # if current_room == state.current_room and is_locked_room.is_locked != "True":
         #     print("no")
@@ -117,8 +130,8 @@ def parse(inputCommand):
                     item.room_id = state.current_room
                     # The Soup Puzzle
                     if item in SoupPuzzle.goal_items:
-                        update_descriptions(SoupPuzzle)   
-                        
+                        update_descriptions(SoupPuzzle)
+
             print("If you say so.")
         except ValueError:
             print(item_name + " is not in your inventory!")
@@ -128,15 +141,18 @@ def parse(inputCommand):
             print("--" + i)
 
     elif command != "quit" and command != "leave":
-        confusion = ["Gesundheit!", "Uhhhhhhhh...What?", "Come again?", "Pardon?", "Did your cat walk across your keyboard?", "I don't understand.", "Go fish."]
+        confusion = ["Gesundheit!", "Uhhhhhhhh...What?", "Come again?", "Pardon?",
+                     "Did your cat walk across your keyboard?", "I don't understand.", "Go fish."]
         print(random.choice(confusion))
 
     if soup_puzzle_complete(SoupPuzzle):
         get_room_object("fridge").lock_room(False)
-        get_room_object("kitchen").set_look("On the stove, there is a pot of soup cooking and some pasta boiling. The soup appears to be complete. Yum.")
-        
+        get_room_object("kitchen").set_look(
+            "On the stove, there is a pot of soup cooking and some pasta boiling. The soup appears to be complete. Yum.")
+
     return True
 ##
+
 
 # Maybe give option to repeat police_report
 print(coloredIntro)
